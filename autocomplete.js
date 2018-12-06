dd32b9227602488bef29731f1552e8a8589fa991
memuunkie <6616957+memuunkie@users.noreply.github.com>
2018-12-05 16:11:06 -0800

    Workaround for _renderItem formatting to bold matching chars

--- autocomplete.js
+++ autocomplete.js
@@ -6,3 +6,23 @@ function handleAutocomplete(request, response) {
 			response(data);
 		});
 }
+
+var at = 
+	$(".get-search-autocomplete-suggestions").autocomplete({
+		source: handleAutocomplete,
+		select: function (event, ui) {
+			$(".search-form").trigger("submit"); // find id, run search on submit
+			// TODO: run search on mouse click
+		}
+	});
+
+at.data("ui-autocomplete")._renderItem = function (ul, item) {
+	var theVal = at.val();
+	// workaround to feed renderItem for bolding matching characters without weirdo formatting
+	var wrapper = $("<div>")
+		.append("<strong>" + theVal + "</strong>" + item.label.substring(theVal.length));
+	return $("<li>")
+		.attr("data-value", item.value)
+		.append(wrapper)
+		.appendTo(ul);
+};
